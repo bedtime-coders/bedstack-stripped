a3baee82adc17fabd0c32f48a334d4eefcc39596
Yam Borodetsky <yam@yam.codes>
2025-06-16 00:30:24 +0500

    Refactor user authentication and registration endpoints in index.ts by grouping routes under a "/users" path; enhance code readability and maintainability while preserving existing functionality for user login and creation.

--- src/index.ts
+++ src/index.ts
@@ -10,106 +10,108 @@ import { StatusCodes } from "http-status-codes";
 import { pick } from "radashi";
 import { errors } from "./plugins/errors";
 
-const api = new Elysia({ prefix: "api" })
-	.use(auth())
-	.post(
-		"/users/login",
-		async ({ body: { user }, auth: { sign } }) => {
-			const [foundUser] = await db
-				.select()
-				.from(users)
-				.where(eq(users.email, user.email))
-				.limit(1);
-			if (!foundUser) {
-				throw new NotFoundError("user");
-			}
-			if (!(await Bun.password.verify(user.password, foundUser.password))) {
-				throw new RealWorldError(StatusCodes.UNAUTHORIZED, {
-					user: ["invalid credentials"],
-				});
-			}
-			return {
-				user: {
-					token: await sign(pick(foundUser, ["id", "email", "username"])),
-					...pick(foundUser, ["email", "username", "bio", "image"]),
-				},
-			};
-		},
-		{
-			detail: {
-				summary: "Authentication",
+const api = new Elysia({ prefix: "/api" }).use(auth()).group("/users", (app) =>
+	app
+		.post(
+			"/login",
+			async ({ body: { user }, auth: { sign } }) => {
+				const [foundUser] = await db
+					.select()
+					.from(users)
+					.where(eq(users.email, user.email))
+					.limit(1);
+				if (!foundUser) {
+					throw new NotFoundError("user");
+				}
+				if (!(await Bun.password.verify(user.password, foundUser.password))) {
+					throw new RealWorldError(StatusCodes.UNAUTHORIZED, {
+						user: ["invalid credentials"],
+					});
+				}
+				return {
+					user: {
+						token: await sign(pick(foundUser, ["id", "email", "username"])),
+						...pick(foundUser, ["email", "username", "bio", "image"]),
+					},
+				};
 			},
-			body: t.Object({
-				user: t.Object({
-					email: t.String({
-						format: "email",
-						minLength: 1,
-						examples: ["jake@jake.jake"],
-					}),
-					password: t.String({
-						minLength: 1,
-						examples: ["hunter2A"],
+			{
+				detail: {
+					summary: "Authentication",
+				},
+				body: t.Object({
+					user: t.Object({
+						email: t.String({
+							format: "email",
+							minLength: 1,
+							examples: ["jake@jake.jake"],
+						}),
+						password: t.String({
+							minLength: 1,
+							examples: ["hunter2A"],
+						}),
 					}),
 				}),
-			}),
-		},
-	)
-	.post(
-		"/users",
-		async ({ body: { user }, auth: { sign } }) => {
-			const [createdUser] = await db
-				.insert(users)
-				.values(user)
-				.onConflictDoNothing()
-				.returning();
-			if (!createdUser) {
-				// TODO: consider selecting, and returning which field conflicted
-				throw new RealWorldError(StatusCodes.CONFLICT, {
-					user: ["already exists"],
-				});
-			}
-			return {
-				user: {
-					token: await sign(pick(createdUser, ["id", "email", "username"])),
-					...pick(createdUser, ["email", "username", "bio", "image"]),
-				},
-			};
-		},
-		{
-			detail: {
-				summary: "Registration",
 			},
-			body: t.Object({
-				user: t.Object({
-					email: t.String({
-						format: "email",
-						examples: ["jake@jake.jake"],
-					}),
-					password: t.String({
-						minLength: 8,
-						maxLength: 100,
-						pattern: "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-Z\\d@$!%*?&]{8,}$",
-						description:
-							"must be at least 8 characters and contain uppercase, lowercase, and numbers",
-						examples: ["hunter2A"],
-					}),
-					username: t.String({ minLength: 2, examples: ["jake"] }),
-					bio: t.Optional(
-						t.String({
-							minLength: 2,
-							examples: ["I work at statefarm"],
+		)
+		.post(
+			"/",
+			async ({ body: { user }, auth: { sign } }) => {
+				const [createdUser] = await db
+					.insert(users)
+					.values(user)
+					.onConflictDoNothing()
+					.returning();
+				if (!createdUser) {
+					// TODO: consider selecting, and returning which field conflicted
+					throw new RealWorldError(StatusCodes.CONFLICT, {
+						user: ["already exists"],
+					});
+				}
+				return {
+					user: {
+						token: await sign(pick(createdUser, ["id", "email", "username"])),
+						...pick(createdUser, ["email", "username", "bio", "image"]),
+					},
+				};
+			},
+			{
+				detail: {
+					summary: "Registration",
+				},
+				body: t.Object({
+					user: t.Object({
+						email: t.String({
+							format: "email",
+							examples: ["jake@jake.jake"],
 						}),
-					),
-					image: t.Optional(
-						t.String({
-							format: "uri",
-							examples: ["https://api.realworld.io/images/smiley-cyrus.jpg"],
+						password: t.String({
+							minLength: 8,
+							maxLength: 100,
+							pattern:
+								"^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-Z\\d@$!%*?&]{8,}$",
+							description:
+								"must be at least 8 characters and contain uppercase, lowercase, and numbers",
+							examples: ["hunter2A"],
 						}),
-					),
+						username: t.String({ minLength: 2, examples: ["jake"] }),
+						bio: t.Optional(
+							t.String({
+								minLength: 2,
+								examples: ["I work at statefarm"],
+							}),
+						),
+						image: t.Optional(
+							t.String({
+								format: "uri",
+								examples: ["https://api.realworld.io/images/smiley-cyrus.jpg"],
+							}),
+						),
+					}),
 				}),
-			}),
-		},
-	);
+			},
+		),
+);
 
 const app = new Elysia().use(errors).use(openapi).use(api).listen(env.PORT);
 
