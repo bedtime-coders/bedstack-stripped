e69ce4340a54555bc1027cf7f103e3633adb4ed5
Yam Borodetsky <yam@yam.codes>
2025-06-16 23:37:00 +0500

    Implement database reset functionality using drizzle-orm
    - Added reset functionality to reset.ts, utilizing the drizzle-orm library for database operations.
    - Updated logging to indicate the completion of the database reset process.
    - Referenced an issue in the drizzle-orm repository for context on the implementation.

--- drizzle/reset.ts
+++ drizzle/reset.ts
@@ -1,7 +1,12 @@
-import { db } from "@/core/db";
 import { users } from "@/users/users.schema";
 import chalk from "chalk";
+import { reset } from "drizzle-seed";
+import { drizzle } from "drizzle-orm/bun-sqlite";
+import { env } from "@/core/env";
 
 console.log(chalk.gray("Resetting database"));
-await db.delete(users);
+// See: https://github.com/drizzle-team/drizzle-orm/issues/3599
+await reset(drizzle(env.DATABASE_URL), {
+	users,
+});
 console.log(`[${chalk.green("✓")}] Database reset complete`);
