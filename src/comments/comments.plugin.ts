dfbceb12000e81a99c6322c3523385b044a0e075
Yam Borodetsky <yam@yam.codes>
2025-06-20 02:38:41 +0500

    Remove UUID validation function and regex from comments plugin for cleaner code.

--- src/comments/comments.plugin.ts
+++ src/comments/comments.plugin.ts
@@ -21,14 +21,6 @@ import {
 	toCommentsResponse,
 } from "./mappers/to-response.mapper";
 
-// UUID validation regex
-const UUID_REGEX =
-	/^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
-
-function isValidUUID(uuid: string): boolean {
-	return UUID_REGEX.test(uuid);
-}
-
 export const commentsPlugin = new Elysia({
 	tags: ["Comments"],
 })
