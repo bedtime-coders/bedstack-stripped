970aa16300adce5d8972218092d0799fb5134d40
Yam Borodetsky <yam@yam.codes>
2025-06-28 13:43:38 +0500

    Refactor database schema management by consolidating relations
    This commit simplifies the database schema management by removing individual schema imports and replacing them with a centralized `relations` import. Additionally, the backup relations file has been deleted, streamlining the codebase and enhancing maintainability.

--- src/core/database/db.ts
+++ src/core/database/db.ts
@@ -1,37 +1,8 @@
 import { drizzle } from "drizzle-orm/bun-sql";
-import {
-	articles,
-	articlesRelations,
-	favorites,
-	favoritesRelations,
-} from "@/articles/articles.schema";
-import { comments, commentsRelations } from "@/comments/comments.schema";
-import { follows, followsRelations } from "@/profiles/profiles.schema";
-import {
-	articlesToTags,
-	articlesToTagsRelations,
-	tags,
-	tagsRelations,
-} from "@/tags/tags.schema";
-import { users, usersRelations } from "@/users/users.schema";
 import { env } from "../env";
+import { relations } from "./relations";
 
 export const db = drizzle(env.DATABASE_URL, {
-	schema: {
-		users,
-		follows,
-		articles,
-		tags,
-		articlesToTags,
-		favorites,
-		comments,
-		articlesRelations,
-		tagsRelations,
-		articlesToTagsRelations,
-		favoritesRelations,
-		commentsRelations,
-		followsRelations,
-		usersRelations,
-	},
+	relations,
 	logger: env.LOG_LEVEL === "debug",
 });
